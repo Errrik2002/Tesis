7fe746d31f25f6c2fa52eb2d48fe8e73172d8b0c
Errrik2002 <119984425+Errrik2002@users.noreply.github.com>
2025-03-20 13:19:41 -0600

    ANalisis de sensibilidad parametrica
    S empezo a hacer analisis de sensibilidad parametrica y se hace aumentando el 10% de cada parametro y corriendo de nuevo el modelo

--- Escrito de tesis/Modelo_DeSolve_29_NOV_2024.R
+++ Escrito de tesis/Modelo_DeSolve_29_NOV_2024.R
@@ -1,3 +1,5 @@
+#install.packages("tidyverse")
+library(tidyverse)
 library(deSolve)
 
 
@@ -51,11 +53,11 @@ trypanosoma_29_NOV <- function(t,state,parameter){
 
 
 parameter <- c(alpha1,alpha2, mu1, nu1, mu2, mu3, mu4, alpha3, alpha4, mu5, mu6, alpha5, alpha7, mu7, mu9, n1, n2, h1, h2, qIL10, qTNF,M0) 
-state <- c(TL=50, M=209, Cn= 136, Mi= 0, Ci = 0, TNF = 0.14, IL10 =0.15)
+state <- c(TL=50, M=209, Cn= 136, Mi= 0, Ci = 0, TNF = 22, IL10 =9.8)
 times <- seq(0,3650, by=1)
-cuatro_de_marzo_2025 <- ode(y= state, times = times, func= trypanosoma_29_NOV, parms = parameter)
+parametros_normales_18_mar_2025 <- ode(y= state, times = times, func= trypanosoma_29_NOV, parms = parameter)
 
-plot(cuatro_de_marzo_2025,col='red')
+plot(parametronormales,col='red')
 
 
 
@@ -75,8 +77,12 @@ legend("topright", c("d_TL", "D_M", "d_Cn", "d_Mi", "d_Ci","d_TNF","d_IL10" ), c
 
 
                                  #### ANALISIS DE SENSIBILIDAD PARAMETRICA ####
+
 Param_Normal #Modelado con parametros normales 
-saveRDS(Param_Normal, "objetosRDS/Parametrosnormales.rds")
+saveRDS(parametros_normales_18_mar_2025, "objetosRDS/Parametrosnormales.rds")
+parametronormales <- readRDS("objetosRDS/Parametrosnormales.rds")
+View(parametronormales)
+
 saveRDS(Param_alpha1_3p14, "objetosRDS/Paramtro_alpha1_3p14.rds")
 saveRDS(Param_mu1_1x10m1, "objetosRDS/parametro_mu1_1x10m1.rds")
 saveRDS(Param_nu1_1080728, "objetosRDS/parametro_nu_1080728.rds")
@@ -89,22 +95,79 @@ saveRDS(Param_alpha7_1p29, "objetosRDS/parametro_alpha7_1p29.rds")
 
 
 
-out29_NOV[3650,]
-#Cambiar los parametros y hacer las diferencias 
-diferencias <- (out29_NOV[3650,])-(out29_NOV[2000,]) #esta son las diferencias
 
 
-out29_NOV[3650,]
+  
+############################################################# 
+######################## 18 DE MARZO DEL 2025 ###############
+#############################################################
 
-plot(x = out29_NOV[3650,1], y = out29_NOV[3650,2])
 
-#HACER UNA BASE DE DATOS DE LOS VALORES DE LAS VARIABLES CON LOS CMBIOS DE LOS PARAMETROS 
 
-plot(out29_NOV[,7])
-  
-  
-  
+abs(-20) #Valor absoluto es la distancia de un valor a otro sin importar el signo, tipo
+#si es -20, implica que hay 20 lugares de diferencia del 0, no importa si es + o -
 
+#Ciclo FOR
+
+#Parametro 1
+
+######################################################
+#CICLO FOR PARA ANALISIS DE SENSIBILIDAD PARAMETRICA ########################################################################
+#############################################################################################################################
+#####################################################
+
+mu2 <- 5*10**-1 #5
+mu2_2 <- 5*10**-1 #5
+
+head(parametronormales)
+
+basedatos_variables <- data.frame(parametro = mu2_2,
+                                  variable_TL_mu2=parametronormales[3651,2])
+basedatos_variables
+
+a<- 0.10
+for (x in 1:10){
   
+  parameter <- c(alpha1,alpha2, mu1, nu1, mu2, mu3, mu4, alpha3, alpha4, mu5, mu6, alpha5, alpha7, mu7, mu9, n1, n2, h1, h2, qIL10, qTNF,M0) 
+  state <- c(TL=50, M=209, Cn= 136, Mi= 0, Ci = 0, TNF = 22, IL10 =9.8)
+  times <- seq(0,3650, by=1)
+
+    
+  trypanosoma_29_NOV <- function(t,state,parameter){
+    with(as.list(c(state,parameter)),  {
+      d_TL <- (alpha1*((TNF**h3)/((n3**h3)+TNF**h3))*(n4**h4)/((n4**h4)+IL10**h4))*TL*M-alpha2*TL*Cn+(mu1*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*TL+(alpha3*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*Mi+alpha4*Ci
+      d_M <- (nu1*((TNF**h1)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*(M0-M)+(alpha1*((TNF**h3)/((n3**h3)+TNF**h3))*(n4**h4)/((n4**h4)+IL10**h4))*TL*M+(mu2*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*M
+      d_Cn <- -alpha2*TL*Cn+(mu3*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*Cn
+      d_Mi <- (alpha1*((TNF**h3)/((n3**h3)+TNF**h3))*(n4**h4)/((n4**h4)+IL10**h4))*TL*M+(mu5*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*Mi
+      d_Ci <- alpha2*TL*Cn+(mu6*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*Ci
+      d_TNF <- (alpha5*((n1**h1)/((n1**h1)+IL10**h1)))*Mi-mu7*(TNF-qTNF)+(alpha5*((n1**h1)/((n1**h1)+IL10**h1)))*Ci
+      d_IL10 <- alpha7*Mi - mu9*(IL10-qIL10)
+      
+      return(list(c(d_TL,d_M,d_Cn,d_Mi,d_Ci,d_TNF,d_IL10)))      
+    })
   
-  
+    
+  }
+dieciocho_deMarzo_2025 <- ode(y= state, times = times, func= trypanosoma_29_NOV, parms = parameter)
+
+
+basedatos_variables[nrow(basedatos_variables)+1,]<- c(mu2,parametronormales[3651,2]/dieciocho_deMarzo_2025[3651,2]) #aqui va el que se ira moviendo 
+
+mu2<- mu2+ (mu2_2*a) #aqui va el que no se va a amover alpha2_2
+#igual aqui ira el que se ira moviendo
+
+
+if (x ==10){
+print(basedatos_variables)
+}
+
+}
+#####
+base_TL_mu2 <-basedatos_variables[-1,]
+base_TL_mu2 
+plot(base_TL_mu2)
+saveRDS(base_TL_mu2, "Tabla_parametros_RDS/Base_TL_mu2.rds")
+#Cada que acabemos un parametro, volverlo a poner en su valor inicial
+
+
+
