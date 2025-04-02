b29c9ad387e1f04939b6c9071ac9fdc853e316c7
Errrik2002 <119984425+Errrik2002@users.noreply.github.com>
2025-04-02 16:28:07 -0600

    Limpieza de Script y lectura de datos
    Realice un acomodo del codigo, asi como sus respectivas descripciones. De igual forma se leyeron datos RDS y se le agrego underbrace a las formulas del 3er modelo

--- Escrito de tesis/Modelo_DeSolve_29_NOV_2024.R
+++ Escrito de tesis/Modelo_DeSolve_29_NOV_2024.R
@@ -1,41 +1,50 @@
-#install.packages("tidyverse")
+#Erik David Martinez Rubio
+#Modelo Matematico de Inmunopatologia de la Enfermedad de Chagas
+#Tesis de Licenciatura en Microbiologia
+
+
+#Instalacion de paquetes necesarios para el modelo
+install.packages("tidyverse")
+install.packages("tidyverse")
+
+#Cargar los paquetes
 library(tidyverse)
 library(deSolve)
 
 
 
-#Parametros
-alpha1 <- 5.8*10**-8 #1 o 3.14, ELISA D H
+
+#Creacion de paramaetros y asignacion de valores a cada uno
+alpha1 <- 5.8*10**-8 #1
 alpha2 <- 2.5*10**-9 #2
-#mu1 <- 0.06 # REFERENCIA DE YANG 2015 (HAY OTRO VALOR)
 mu1 <- 0.06 #3
-nu1 <- 1 # REFERENCIA DE FREITAS, LA OTRA OPCION ES MUY DRASTICA  #4 
+nu1 <- 1 #4
 mu2 <- 5*10**-1 #5
-mu3 <- 0 #MISMO VALOR QUE EL DE LOS MACROFAGOS, MU2 #6
-alpha3 <- 0 #replicacion Mi #8 PUEDE VARIAR SI ES M1 O M2
+mu3 <- 0 #6
+alpha3 <- 0
 alpha4 <- 90 #Replicacion Ci #9
-mu5 <- 5*10**-1 #10 #NO AFECTAN, PUEDE SER POR LAS CITOCINAS
+mu5 <- 5*10**-1 #10
 mu6 <- 1*10**-6 #11
 alpha5 <- 14.4 #12
-alpha7 <- 0.45 #SECRECION DE IL10 #13
-mu7 <- 24 #TASA DE degradacion DE TNF #en base a la literatura CADA 18 MIN #14 #24
-mu9 <- 19.2 #15 Degradacion de IL10
-n1 <- 17.4 #TNF-IL10
-n2 <- 560 #IL10-IL6
-h1 <- 3 #TNF-IL10
-h2 <- 3.68 #IL10-IL6
-M0 <-209# NUMERO EQUIS
-qTNF <- 0.14 #16
-qIL10 <- 0.15 #17  
-n3 <- 100 #MTNF
-h3 <- 3.16 #MTNF
-n4 <- 4.35 #MIL10
-h4 <- 0.3#MIL10
+alpha7 <- 0.45 #13
+mu7 <- 24 #14
+mu9 <- 19.2 #15
+n1 <- 17.4 #TNF-IL10 #16
+n2 <- 560 #IL10-IL6 #17
+h1 <- 3 #TNF-IL10 #18
+h2 <- 3.68 #IL10-IL6 #19
+M0 <-209# NUMERO EQUIS #20
+qTNF <- 0.14  #21
+qIL10 <- 0.15 #22  
+n3 <- 100 #MTNF #23
+h3 <- 3.16 #MTNF #24
+n4 <- 4.35 #MIL10 #25
+h4 <- 0.3#MIL10 #26
 
 
-    
-#    return(list(c(d_TL,d_M,d_Cn,d_Mi,d_Ci,d_TNF,d_IL10)))
-
+#################################################
+################################################
+#Se hace la funcion para graficar el modelo, en la cual se le agrega el sistema de ecuaciones
 
 trypanosoma_29_NOV <- function(t,state,parameter){
   with(as.list(c(state,parameter)),  {
@@ -44,35 +53,29 @@ trypanosoma_29_NOV <- function(t,state,parameter){
     d_Cn <- -alpha2*TL*Cn-(mu3*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*Cn
     d_Mi <- (alpha1*((TNF**h3)/((n3**h3)+TNF**h3))*(n4**h4)/((n4**h4)+IL10**h4))*TL*M-(mu5*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*Mi
     d_Ci <- alpha2*TL*Cn-(mu6*((TNF**h3)/((n3**h3)+TNF**h3))*((n4**h4)/((n4**h4)+IL10**h4)))*Ci
-    d_TNF <- (alpha5*((n1**h1)/((n1**h1)+IL10**h1)))*Mi-mu7*(TNF-qTNF)+((alpha5*((n1**h1)/((n1**h1)+IL10**h1)))*Ci)#*0.25
+    d_TNF <- (alpha5*((n1**h1)/((n1**h1)+IL10**h1)))*Mi-mu7*(TNF-qTNF)+((alpha5*((n1**h1)/((n1**h1)+IL10**h1)))*Ci)
     d_IL10 <- alpha7*Mi - mu9*(IL10-qIL10)
 
     return(list(c(d_TL,d_M,d_Cn,d_Mi,d_Ci,d_TNF,d_IL10)))      
   })
 }
 
+# Se asignan los parametros, estados iniciales, el tiempo que se quiere modelar
+#Y se genera soluciona el sistema de ecuaciones
 
 parameter <- c(alpha1,alpha2, mu1, nu1, mu2, mu3, alpha3, alpha4, mu5, mu6, alpha5, alpha7, mu7, mu9, n1, n2, h1, h2, qIL10, qTNF,M0) 
 state <- c(TL=50, M=209, Cn= 136, Mi= 0, Ci = 0, TNF = 22, IL10 =9.8)
-times <- seq(0,3650, by=1)
+times <- seq(0,5000, by=1)
 parametros_signos_bien_27_mar_nosave <- ode(y= state, times = times, func= trypanosoma_29_NOV, parms = parameter)
 
+# Se grafican las soluciones
 plot(parametros_signos_bien_27_mar_nosave,col='red')
 
-
+#Se guardan las soluciones del sistema de ecuaciones 
 saveRDS(parametros_signos_bien_27_mar, "analisis_sensibilidad_param_signos_bien/parametros_normales_signos_bien.rds")
 signos_bien_parametros_normales <- readRDS("analisis_sensibilidad_param_signos_bien/parametros_normales_signos_bien.rds")
 
-#parametros_normales_18_mar_2025[3651,]
-#parametronormales[3651,]
-
-
-   #############Guardar las imagenes ###########
-
-#pdf("images/Parametros_normales_28_mar_25.pdf",width = 6, height = 8)
-#plot(parametros_signos_bien_27_mar_nosave,col='red')
-#dev.off()
-   ##################################
+  
 
 
 
@@ -342,6 +345,52 @@ analisis_SP(M0,21,8)
 
 
 
+########################## leer los RDS para saber cuanto se afecto la variable
 
+alpha1_Macrofagos_inf <- readRDS("analisis_sensibilidad_param_signos_bien/alpha1_Mi.rds")
+alpha1_Macrofagos_inf
+##
 
+alpha2_cardiomiocitos_no_infec <- readRDS("analisis_sensibilidad_param_signos_bien/alpha2_Cn.rds")
+alpha2_cardiomiocitos_no_infec
+##
+alpha4_Cardiomiocito_no_infec <- readRDS("analisis_sensibilidad_param_signos_bien/alpha4_Cn.rds")
+alpha4_Cardiomiocito_no_infec
 
+alpha4_Macrofagos_infectados <- readRDS("analisis_sensibilidad_param_signos_bien/alpha4_Mi.rds")
+alpha4_Macrofagos_infectados
+
+alpha4_trypanosoma_libre <- readRDS("analisis_sensibilidad_param_signos_bien/alpha4_TL.rds")
+alpha4_trypanosoma_libre
+##
+alpha5_cardiomiocito_no_infec <- readRDS("analisis_sensibilidad_param_signos_bien/alpha5_Cn.rds")
+alpha5_cardiomiocito_no_infec
+
+alpha5_macrofagos_infectados <- readRDS("analisis_sensibilidad_param_signos_bien/alpha5_Mi.rds")
+alpha5_macrofagos_infectados
+
+alpha5_parasitos_libres <- readRDS("analisis_sensibilidad_param_signos_bien/alpha5_TL.rds")
+alpha5_parasitos_libres
+
+alpha5_TNF <- readRDS("analisis_sensibilidad_param_signos_bien/alpha5_TNF.rds")
+alpha5_TNF
+##
+
+alpha7_IL10<- readRDS("analisis_sensibilidad_param_signos_bien/alpha7_IL10.rds")
+alpha7_IL10
+##
+
+mu5_macrofagos_infectados <- readRDS("analisis_sensibilidad_param_signos_bien/mu5_Mi.rds")
+mu5_macrofagos_infectados
+##
+mu7_cardiomiocito_no_infectado <- readRDS("analisis_sensibilidad_param_signos_bien/mu7_Cn.rds")
+mu7_cardiomiocito_no_infectado
+
+mu7_IL10 <- readRDS("analisis_sensibilidad_param_signos_bien/mu7_IL10.rds")
+mu7_IL10
+
+mu7_Macrofagos_infectados <- readRDS("analisis_sensibilidad_param_signos_bien/mu7_Mi.rds")
+mu7_Macrofagos_infectados
+
+mu7_tripanosomas_libres <- readRDS("analisis_sensibilidad_param_signos_bien/mu7_TL.rds")
+mu7_tripanosomas_libres
